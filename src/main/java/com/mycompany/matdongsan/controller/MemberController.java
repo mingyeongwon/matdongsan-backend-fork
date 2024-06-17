2d2ed435ae9d23f954b1ef2121f092c673c43efe
sunghwan kwon <sunghwan.ian.kwon@gmail.com>
2024-06-17 20:42:16 +0900

    deleted vue files in boot, added comments in controller files

--- src/main/java/com/mycompany/matdongsan/controller/MemberController.java
+++ src/main/java/com/mycompany/matdongsan/controller/MemberController.java
@@ -7,5 +7,9 @@ import lombok.extern.slf4j.Slf4j;
 @Controller
 @Slf4j
 public class MemberController {
-
+	//로그인
+	//회원가입
+	//탈퇴
+	//비밀번호 수정
+	// 
 }
--- src/main/java/com/mycompany/matdongsan/controller/PropertyController.java
+++ src/main/java/com/mycompany/matdongsan/controller/PropertyController.java
@@ -7,5 +7,7 @@ import lombok.extern.slf4j.Slf4j;
 @Controller
 @Slf4j
 public class PropertyController {
-
+	//부동산
+	//매물
+	
 }
