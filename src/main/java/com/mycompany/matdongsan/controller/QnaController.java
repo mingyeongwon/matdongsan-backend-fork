3e421d7f4746837d15866b91a8d4445c0c99c316
soo <hongmoon0430@gmail.com>
2024-06-24 21:50:07 +0900

    added customerInquiry in Question controller

--- src/main/java/com/mycompany/matdongsan/controller/QnaController.java
+++ src/main/java/com/mycompany/matdongsan/controller/QnaController.java
@@ -1,11 +1,55 @@
 package com.mycompany.matdongsan.controller;
 
-import org.springframework.stereotype.Controller;
+import java.io.IOException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.mycompany.matdongsan.dto.Question;
+import com.mycompany.matdongsan.service.QnaService;
 
 import lombok.extern.slf4j.Slf4j;
 
-@Controller
+@RestController
 @Slf4j
+@RequestMapping("/Qna")
 public class QnaController {
-	//공지사항
+	@Autowired 
+	private QnaService qnaService;
+	
+	@PostMapping("/CustomerInquiryForm")
+	public Question create(Question question) {
+		if(question.getQattach() != null && !question.getQattach().isEmpty()) {
+			log.info("첨부 파일 있음");
+			
+			MultipartFile mf = question.getQattach();
+			
+			question.setQattachoname(mf.getOriginalFilename());
+			question.setQattachtype(mf.getContentType());
+			try {
+				question.setQattachdata(mf.getBytes());
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+						
+		} else {
+			log.info("첨부 파일 없음");
+		}
+		
+//		question.setQEmail(authentication.getName());
+		question.setQEmail("user");
+		log.info(question.toString()); // postman으로 볼 수 없는 multipartfile이나 byte배열을 확인 
+		
+		qnaService.insert(question);
+		
+		// json 반환할 때 json으로 변환할 수 없는 multipartfile이나 byte배열같은 타입은 빼야 한다.
+		question.setQattach(null);
+		question.setQattachdata(null);
+		return question;
+	}
 }
--- src/main/java/com/mycompany/matdongsan/dao/QuestionDao.java
+++ src/main/java/com/mycompany/matdongsan/dao/QuestionDao.java
@@ -2,7 +2,10 @@ package com.mycompany.matdongsan.dao;
 
 import org.apache.ibatis.annotations.Mapper;
 
+import com.mycompany.matdongsan.dto.Question;
+
 @Mapper
 public interface QuestionDao {
-
+	
+	public int insert(Question question);
 }
