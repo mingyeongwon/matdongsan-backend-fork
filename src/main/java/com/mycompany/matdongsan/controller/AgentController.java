8cd9fa6e3dbe0917400de229b4fd1f2c7fd4556b
sunghwan kwon <sunghwan.ian.kwon@gmail.com>
2024-06-25 15:58:44 +0900

    updated db address

--- src/main/java/com/mycompany/matdongsan/controller/AgentController.java
+++ src/main/java/com/mycompany/matdongsan/controller/AgentController.java
@@ -1,5 +1,10 @@
 package com.mycompany.matdongsan.controller;
 
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
@@ -7,9 +12,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.mycompany.matdongsan.dto.Agent;
+import com.mycompany.matdongsan.dto.AgentDetail;
 import com.mycompany.matdongsan.service.AgentService;
 
 import lombok.extern.slf4j.Slf4j;
@@ -20,19 +28,45 @@ public class AgentController {
 
 	@Autowired
 	private AgentService agentService;
-
 	// 부동산 정보 리스트 출력
 	@GetMapping("/Agent")
-	public String GetAgentList() {
+	public Map<String,Object> GetAgentList(@RequestParam(defaultValue = "1") int pageNo,@RequestParam(defaultValue = "agnet") String page) {
 		log.info("실행");
+		// 무한 스크롤
+		List<Agent> list = agentService.getAgentList();
+		
+		Map<String,Object> map = new HashMap<>();
+		map.put("agent", list);
+		//무한 스크롤을 리턴값으로 넘겨야함
 		return null;
 	}
 
 	// 부동산 등록
+	// 리턴값과 파라미터 값으로 agent와 agentDetail이 합쳐진 dto를 받아야함
 	@PostMapping("/Signup/AgentSignup")
-	public Agent createAgentAccount(Agent agent, Authentication authentication) {
+	public AgentDetail createAgentAccount(AgentDetail agentDetail,Agent agent, Authentication authentication) {
+		//첨부가 넘어왔을 경우 처리
+		if (agentDetail.getAdattach() != null && !agentDetail.getAdattach().isEmpty()) {
+			MultipartFile mf = agentDetail.getAdattach();
+			// 파일 이름을 설정
+			agentDetail.setAdattachoname(mf.getOriginalFilename());
+			// 파일 종류를 설정
+			agentDetail.setAdattachtype(mf.getContentType());
+			try {
+				// 파일 데이터를 설정
+				agentDetail.setAdattachdata(mf.getBytes());
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		// DB에 저장
+		agent.setAemail(authentication.getName());
+		agentService.insertAgentData(agent,agentDetail);
+		// JSON으로 변환되지 않는 필드(attach,attach data등)는 null 처리
+		agentDetail.setAdattach(null);
+		agentDetail.setAdattachdata(null);
 
-		return null;
+		return agentDetail;
 	}
 
 	// 매물 상세
@@ -54,10 +88,10 @@ public class AgentController {
 		return null;
 	}
 	// 마이페이지 부동산 중개업자 정보 수정
-	@PreAuthorize("hasAuthority('ROLE_AGENT')") // 유저일 경우에만 등록 가능
+	@PreAuthorize("hasAuthority('ROLE_USER')") // 중개인일 경우에만 등록 가능
 	@GetMapping("/Mypage/MyInfomation")
 	public Agent getMypagePropertyInfoList(@PathVariable int aid) {
-
+		
 		return null;
 	}
 
--- src/main/java/com/mycompany/matdongsan/service/AgentService.java
+++ src/main/java/com/mycompany/matdongsan/service/AgentService.java
@@ -1,8 +1,29 @@
 package com.mycompany.matdongsan.service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
+
+import com.mycompany.matdongsan.dto.Agent;
+import com.mycompany.matdongsan.dto.AgentDetail;
 
 @Service
 public class AgentService {
+	//agent 데이터 카운트
+	public int getCount() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	
+	//Agent 데이터 리스트 가져오기
+	public List<Agent> getAgentList() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void insertAgentData(Agent agent,AgentDetail agentDetail) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
