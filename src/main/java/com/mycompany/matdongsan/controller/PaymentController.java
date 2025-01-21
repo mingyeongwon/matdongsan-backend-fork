cc179d97f853ce75c0d6f36102e9101442056123
mingyeongwon <wonmingy@gmail.com>
2025-01-21 22:57:02 +0900

    feat: PaymentController 작성

---
+++ src/main/java/com/mycompany/matdongsan/controller/PaymentController.java
@@ -0,0 +1,41 @@
+package com.mycompany.matdongsan.controller;
+
+import com.siot.IamportRestClient.IamportClient;
+import com.siot.IamportRestClient.exception.IamportResponseException;
+import com.siot.IamportRestClient.response.IamportResponse;
+import com.siot.IamportRestClient.response.Payment;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.PostConstruct;
+import java.io.IOException;
+
+@RestController
+@RequestMapping("/api/payment")
+@Slf4j
+public class PaymentController {
+
+    private IamportClient iamportClient;
+
+    @Value("${imp.apiKey}")
+    private String apiKey;
+
+    @Value("${imp.secretkey}")
+    private String secretKey;
+
+    @PostConstruct
+    public void init() {
+        this.iamportClient = new IamportClient(apiKey, secretKey);
+    }
+
+    @PostMapping("/validate/{imp_uid}")
+    public IamportResponse<Payment> validateIamport(@PathVariable String imp_uid) throws IamportResponseException, IOException {
+        IamportResponse<Payment> payment = iamportClient.paymentByImpUid(imp_uid);
+        log.info("결제 요청 응답. 결제 내역 - 주문 번호: {}", payment.getResponse().getMerchantUid());
+        return payment;
+    }
+}
