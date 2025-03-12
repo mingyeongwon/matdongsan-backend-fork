98dd3d1549d53f5645e15bb711ddd8a81d4c38be
mingyeongwon <wonmingy@gmail.com>
2025-03-13 00:12:29 +0900

    feat: 단일 문자 전송 SERVICE 작성

--- src/main/java/com/mycompany/matdongsan/service/SmsService.java
+++ src/main/java/com/mycompany/matdongsan/service/SmsService.java
@@ -3,6 +3,8 @@ package com.mycompany.matdongsan.service;
 import com.mycompany.matdongsan.dto.SmsRequest;
 import net.nurigo.sdk.NurigoApp;
 import net.nurigo.sdk.message.model.Message;
+import net.nurigo.sdk.message.request.SingleMessageSendingRequest;
+import net.nurigo.sdk.message.response.SingleMessageSentResponse;
 import net.nurigo.sdk.message.service.DefaultMessageService;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -25,6 +27,17 @@ public class SmsService {
 
     public SmsService() {
         this.messageService = NurigoApp.INSTANCE.initialize(apiKey, apiSecret, "https://api.coolsms.co.kr");
+    }
+
+    public void sendSms(SmsRequest requestDto) {
+        String certificationCode = generateCertificationCode();
+
+        Message message = new Message();
+        message.setFrom(fromNumber);
+        message.setTo(requestDto.getPhoneNum());
+        message.setText("인증번호는 " + certificationCode + " 입니다.");
+
+        SingleMessageSentResponse response = this.messageService.sendOne(new SingleMessageSendingRequest(message));
     }
 
     private String generateCertificationCode() {
