35b52f8a38ea8011ed0e7a70340795616af952b0
mingyeongwon <wonmingy@gmail.com>
2025-03-13 00:09:01 +0900

    feat: 인증번호 6자리 생성

--- src/main/java/com/mycompany/matdongsan/service/SmsService.java
+++ src/main/java/com/mycompany/matdongsan/service/SmsService.java
@@ -1,8 +1,14 @@
 package com.mycompany.matdongsan.service;
 
 import com.mycompany.matdongsan.dto.SmsRequest;
+import net.nurigo.sdk.NurigoApp;
+import net.nurigo.sdk.message.model.Message;
+import net.nurigo.sdk.message.service.DefaultMessageService;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+import java.security.SecureRandom;
 
 @Service
 public class SmsService {
@@ -15,7 +21,17 @@ public class SmsService {
     @Value("${coolsms.fromNumber}")
     private String fromNumber;
 
-    public void sendSms(SmsRequest requestDto) {
+    private final DefaultMessageService messageService;
 
+    public SmsService() {
+        this.messageService = NurigoApp.INSTANCE.initialize(apiKey, apiSecret, "https://api.coolsms.co.kr");
     }
+
+    private String generateCertificationCode() {
+        SecureRandom secureRandom = new SecureRandom();
+        int code = secureRandom.nextInt(900000) + 100000;
+        return String.valueOf(code);
+    }
+
+
 }
